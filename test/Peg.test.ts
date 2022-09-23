f918b501eac0ea26865096682f82fdd23afae5de
ok-da <aduko.reverse@gmail.com>
2022-09-23 21:59:51 +0900

    Remove printing

--- test/Peg.test.ts
+++ test/Peg.test.ts
@@ -1,18 +1,9 @@
-import {
-  Nonterminal,
-  NullParsingExpression,
-  parseGrammar,
-  Peg,
-  Rule,
-  Terminal,
-} from '../src';
-import { peToString } from '../src/Printer';
+import { parseGrammar } from '../src';
 
 describe('Peg', () => {
   it('should print a grammar', () => {
     const grammar = `@water foo <- 'a' 'b' / ('c' / 'd')* bar <- "e" r'f'`;
     const peg = parseGrammar(grammar);
-    console.log(peg.toString());
     const output = `@water
 foo <- 
     'a' 'b' /
