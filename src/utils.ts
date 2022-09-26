7afd165c8315b178d246350a51e75808fbb7f232
ok-da <aduko.reverse@gmail.com>
2022-09-27 07:17:11 +0900

    Replace process.hrtime.bigint() with performance.now()

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
+// Copyright (C) 2022- Katsumi Okuda.  All rights reserved.
+
 export function measure<T>(f: () => T): [T, number] {
-  const start = process.hrtime.bigint();
+  const start = performance.now();
   const result = f();
-  const end = process.hrtime.bigint();
-  return [result, Number(end - start)];
+  const end = performance.now();
+  return [result, end - start];
 }
