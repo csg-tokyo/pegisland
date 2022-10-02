f39685eb66b5edee58c26154f8e9dd2224fa07f9
ok-da <aduko.reverse@gmail.com>
2022-10-02 09:09:07 +0900

    Reduce duplicate code

--- src/search.ts
+++ src/search.ts
@@ -5,99 +5,64 @@ function flatten<T>(array: T[][]): T[] {
   return ([] as T[]).concat(...array);
 }
 
-export function inRange(
-  range: Range,
-  startLine: number,
-  startCol: number,
-  endLine: number,
-  endCol: number
-): boolean {
-  if (range.start.line < startLine) {
-    return false;
-  }
-  if (range.end.line > endLine) {
-    return false;
-  }
-  if (range.start.line == startLine && range.start.column < startCol) {
-    return false;
-  }
-  if (range.end.line == endLine && range.end.column > endCol) {
-    return false;
-  }
-  return true;
+interface LineColumn {
+  line: number;
+  column: number;
 }
 
-export function max(
-  startLineX: number,
-  startColX: number,
-  startLineY: number,
-  startColY: number
-): [number, number] {
-  if (startLineX > startLineY) {
-    return [startLineX, startColX];
-  } else if (startLineX < startLineY) {
-    return [startLineY, startColY];
-  }
-  return [startLineX, Math.max(startColX, startColY)];
+interface LineColumnRange {
+  start: LineColumn;
+  end: LineColumn;
 }
 
-export function min(
-  startLineX: number,
-  startColX: number,
-  startLineY: number,
-  startColY: number
-): [number, number] {
-  if (startLineX < startLineY) {
-    return [startLineX, startColX];
-  } else if (startLineX > startLineY) {
-    return [startLineY, startColY];
-  }
-  return [startLineX, Math.min(startColX, startColY)];
+function isGEQ(a: LineColumn, b: LineColumn): boolean {
+  return a.line > b.line || (a.line === b.line && a.column >= b.column);
+}
+
+function isLEQ(a: LineColumn, b: LineColumn): boolean {
+  return isGEQ(b, a);
+}
+
+export function inRange(a: LineColumnRange, b: LineColumnRange): boolean {
+  return isGEQ(a.start, b.start) && isLEQ(a.end, b.end);
+}
+
+function select<T>(f: (a: T, b: T) => boolean, a: T, b: T): T {
+  return f(a, b) ? a : b;
+}
+
+export function max(a: LineColumn, b: LineColumn): LineColumn {
+  return select(isGEQ, a, b);
+}
+
+export function min(a: LineColumn, b: LineColumn): LineColumn {
+  return select(isLEQ, a, b);
 }
 
 export function searchExpressions(
   node: IParseTree,
-  startLine: number,
-  startCol: number,
-  endLine: number,
-  endCol: number
+  range: LineColumnRange
 ): IParseTree[] {
   const nodeIsLeaf = node.childNodes.length == 0;
   if (nodeIsLeaf) {
-    if (inRange(node.range, startLine, startCol, endLine, endCol)) {
-      return [node];
-    } else {
-      return [];
-    }
+    return inRange(node.range, range) ? [node] : [];
   }
   const nodes = flatten(
-    node.childNodes.map((child) =>
-      searchExpressions(child, startLine, startCol, endLine, endCol)
-    )
+    node.childNodes.map((child) => searchExpressions(child, range))
   );
   const nodeSet = new Set(nodes);
-  if (node.childNodes.every((child) => nodeSet.has(child))) {
-    return [node];
-  }
-  return nodes;
+  return node.childNodes.every((child) => nodeSet.has(child)) ? [node] : nodes;
 }
 
 export type SelectedTree = [IParseTree, SelectedTree[]];
 
 export function search(
   node: IParseTree,
-  startLine: number,
-  startCol: number,
-  endLine: number,
-  endCol: number
+  range: LineColumnRange
 ): SelectedTree[] {
-  const trees = flatten(
-    node.childNodes.map((child) =>
-      search(child, startLine, startCol, endLine, endCol)
-    )
-  );
+  const trees = flatten(node.childNodes.map((child) => search(child, range)));
   const isEmpty = node.range.start.equal(node.range.end);
-  if (!isEmpty && inRange(node.range, startLine, startCol, endLine, endCol)) {
+  if (!isEmpty && inRange(node.range, range)) {
     //  if (node instanceof NNonterminal || node instanceof NTerminal) {
     return [[node, trees]];
     //  }
