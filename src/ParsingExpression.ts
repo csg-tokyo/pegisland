66d994bddacbdd9cdba3e755816688ae2bd1a9bf
ok-da <aduko.reverse@gmail.com>
2022-10-08 06:36:35 +0900

    Reorganize classes structure

--- src/ParsingExpression.ts
+++ src/ParsingExpression.ts
@@ -1,141 +1,92 @@
 // Copyright (C) 2021- Katsumi Okuda.  All rights reserved.
-import { IParsingEnv } from './IParsingEnv';
 import { IParsingExpressionVisitor } from './IParsingExpressionVisitor';
-import { IParseTree } from './ParseTree';
-import { Position } from './Position';
 import { Rule } from './Rule';
 
 export interface IParsingExpression {
   accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T;
 }
 
-export class NullParsingExpression implements IParsingExpression {
-  parse(_env: IParsingEnv, _pos: Position): [IParseTree, Position] | null {
-    return null;
-  }
+class ParsingExpression implements IParsingExpression {
+  readonly #name = `visit${(this as object).constructor.name}`;
 
-  accept<T, U>(_visitor: IParsingExpressionVisitor<T, U>, _arg?: U): T {
+  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
+    return visitor[this.#name as keyof typeof visitor](this as never, arg);
+  }
+}
+
+class UnaryOperator extends ParsingExpression {
+  constructor(public operand: IParsingExpression) {
+    super();
+  }
+}
+
+class BinaryOperator extends ParsingExpression {
+  constructor(public lhs: IParsingExpression, public rhs: IParsingExpression) {
+    super();
+  }
+}
+
+class GeneralOperator extends ParsingExpression {
+  constructor(public operands: IParsingExpression[]) {
+    super();
+  }
+}
+
+export class NullParsingExpression extends ParsingExpression {
+  override accept<T, U>(
+    _visitor: IParsingExpressionVisitor<T, U>,
+    _arg?: U
+  ): T {
     throw Error('Should not be called');
   }
 }
 
-export class Nonterminal implements IParsingExpression {
-  constructor(public rule: Rule, public name = '') {}
-
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor[
-      `visit${(this as object).constructor.name}` as keyof typeof visitor
-    ](this as any, arg);
+export class Nonterminal extends ParsingExpression {
+  constructor(public rule: Rule, public name = '') {
+    super();
   }
 }
 
-export class Terminal implements IParsingExpression {
+export class Terminal extends ParsingExpression {
   regex: RegExp;
 
   constructor(public pattern: string | RegExp, public source: string) {
+    super();
     this.regex =
       pattern instanceof RegExp
         ? new RegExp(pattern.source, 'smy')
         : new RegExp(pattern, 'smy');
   }
+}
 
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor.visitTerminal(this, arg);
+export class ZeroOrMore extends UnaryOperator {}
+
+export class OneOrMore extends UnaryOperator {}
+
+export class Optional extends UnaryOperator {}
+
+export class And extends UnaryOperator {}
+
+export class Not extends UnaryOperator {}
+
+export class Colon extends BinaryOperator {}
+
+export class ColonNot extends BinaryOperator {}
+
+export class Sequence extends GeneralOperator {}
+
+export class OrderedChoice extends GeneralOperator {}
+
+export class Grouping extends UnaryOperator {}
+
+export class Rewriting extends ParsingExpression {
+  constructor(public operand: IParsingExpression, public spec: string) {
+    super();
   }
 }
 
-export class ZeroOrMore implements IParsingExpression {
-  constructor(public operand: IParsingExpression) {}
-
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor.visitZeroOrMore(this, arg);
-  }
-}
-
-export class OneOrMore implements IParsingExpression {
-  constructor(public operand: IParsingExpression) {}
-
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor.visitOneOrMore(this, arg);
-  }
-}
-
-export class Optional implements IParsingExpression {
-  constructor(public operand: IParsingExpression) {}
-
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor.visitOptional(this, arg);
-  }
-}
-
-export class And implements IParsingExpression {
-  constructor(public operand: IParsingExpression) {}
-
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor.visitAnd(this, arg);
-  }
-}
-
-export class Not implements IParsingExpression {
-  constructor(public operand: IParsingExpression) {}
-
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor.visitNot(this, arg);
-  }
-}
-
-export class Colon implements IParsingExpression {
-  constructor(public lhs: IParsingExpression, public rhs: IParsingExpression) {}
-
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor.visitColon(this, arg);
-  }
-}
-
-export class ColonNot implements IParsingExpression {
-  constructor(public lhs: IParsingExpression, public rhs: IParsingExpression) {}
-
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor.visitColonNot(this, arg);
-  }
-}
-
-export class Sequence implements IParsingExpression {
-  constructor(public operands: IParsingExpression[]) {}
-
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor.visitSequence(this, arg);
-  }
-}
-
-export class OrderedChoice implements IParsingExpression {
-  constructor(public operands: IParsingExpression[]) {}
-
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor.visitOrderedChoice(this, arg);
-  }
-}
-
-export class Grouping implements IParsingExpression {
-  constructor(public operand: IParsingExpression) {}
-
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor.visitGrouping(this, arg);
-  }
-}
-
-export class Rewriting implements IParsingExpression {
-  constructor(public operand: IParsingExpression, public spec: string) {}
-
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor.visitRewriting(this, arg);
-  }
-}
-
-export class Lake implements IParsingExpression {
+export class Lake extends UnaryOperator {
   semantics: IParsingExpression = new NullParsingExpression();
-
-  constructor(public operand: IParsingExpression) {}
 
   makeSemantics(symbols: Set<IParsingExpression>, waterRules: Rule[]) {
     const operandIsEpsilon =
@@ -160,9 +111,5 @@ export class Lake implements IParsingExpression {
       )
     );
     this.operand = this.semantics;
-  }
-
-  accept<T, U>(visitor: IParsingExpressionVisitor<T, U>, arg?: U): T {
-    return visitor.visitLake(this, arg);
   }
 }
