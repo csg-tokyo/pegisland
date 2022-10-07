9de403fe0c2b9d9b6ef0ed550f3307e36fc6b148
ok-da <aduko.reverse@gmail.com>
2022-10-08 06:45:02 +0900

    Fix lint warnings

--- test/SetCalculator.test.ts
+++ test/SetCalculator.test.ts
@@ -18,31 +18,31 @@ expr_stmt  <- <elake>* ';'
 `;
       const builder = new GeneralPegBuilder();
       builder.build(s);
-      const rules = builder.rules;
+      const {rules} = builder;
 
       const beginningCalculator = new BeginningCalculator(builder.rules);
       beginningCalculator.calculate();
       const beginnings = beginningCalculator.peSet;
-      assert(beginnings.get((rules.get('block') as Rule).rhs)?.size == 1);
-      assert(beginnings.get((rules.get('stmt') as Rule).rhs)?.size == 2);
-      assert(beginnings.get((rules.get('expr_stmt') as Rule).rhs)?.size == 2);
-      assert(beginnings.get((rules.get('<elake>') as Rule).rhs)?.size == 1);
+      assert(beginnings.get((rules.get('block') as Rule).rhs)?.size === 1);
+      assert(beginnings.get((rules.get('stmt') as Rule).rhs)?.size === 2);
+      assert(beginnings.get((rules.get('expr_stmt') as Rule).rhs)?.size === 2);
+      assert(beginnings.get((rules.get('<elake>') as Rule).rhs)?.size === 1);
 
       const succeedCalculator = new SucceedCalculator(rules, beginnings);
       succeedCalculator.calculate();
       const succeeds = succeedCalculator.peSet;
-      assert(succeeds.get((rules.get('block') as Rule).rhs)?.size == 2);
-      assert(succeeds.get((rules.get('stmt') as Rule).rhs)?.size == 2);
-      assert(succeeds.get((rules.get('expr_stmt') as Rule).rhs)?.size == 2);
-      assert(succeeds.get((rules.get('<elake>') as Rule).rhs)?.size == 2);
+      assert(succeeds.get((rules.get('block') as Rule).rhs)?.size === 2);
+      assert(succeeds.get((rules.get('stmt') as Rule).rhs)?.size === 2);
+      assert(succeeds.get((rules.get('expr_stmt') as Rule).rhs)?.size === 2);
+      assert(succeeds.get((rules.get('<elake>') as Rule).rhs)?.size === 2);
 
       const altCalculator = new AltCalculator(rules, beginnings, succeeds);
       altCalculator.calculate();
       const alts = altCalculator.peSet;
-      assert(alts.get((rules.get('block') as Rule).rhs)?.size == 1);
-      assert(alts.get((rules.get('stmt') as Rule).rhs)?.size == 1);
-      assert(alts.get((rules.get('expr_stmt') as Rule).rhs)?.size == 2);
-      assert(alts.get((rules.get('<elake>') as Rule).rhs)?.size == 3);
+      assert(alts.get((rules.get('block') as Rule).rhs)?.size === 1);
+      assert(alts.get((rules.get('stmt') as Rule).rhs)?.size === 1);
+      assert(alts.get((rules.get('expr_stmt') as Rule).rhs)?.size === 2);
+      assert(alts.get((rules.get('<elake>') as Rule).rhs)?.size === 3);
     });
   });
 });
