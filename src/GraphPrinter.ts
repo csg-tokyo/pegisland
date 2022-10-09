c1a6da6c7223f5990b11f4bde4133ba56b9af3e3
ok-da <aduko.reverse@gmail.com>
2022-10-09 23:44:16 +0900

    Modify code to update ts-graphviz

--- src/GraphPrinter.ts
+++ src/GraphPrinter.ts
@@ -1,11 +1,11 @@
 // Copyright (C) 2022- Katsumi Okuda.  All rights reserved.
-import { digraph, INode, toDot } from 'ts-graphviz';
+import { digraph, NodeModel, toDot } from 'ts-graphviz';
 import { Rule } from './Rule';
 import { Peg } from './Peg';
 
 export function genDot(peg: Peg, parentsMap: Map<Rule, Set<Rule>>): string {
   const g = digraph('G');
-  const nodes = new Map<Rule, INode>();
+  const nodes = new Map<Rule, NodeModel>();
   [...peg.rules.values()].forEach((rule) => {
     const node = g.createNode(rule.symbol);
     nodes.set(rule, node);
@@ -13,9 +13,9 @@ export function genDot(peg: Peg, parentsMap: Map<Rule, Set<Rule>>): string {
   [...peg.rules.values()].forEach((rule) => {
     if (parentsMap.has(rule)) {
       const parents = [...(parentsMap.get(rule) as Set<Rule>)];
-      const to = nodes.get(rule) as INode;
+      const to = nodes.get(rule) as NodeModel;
       parents.forEach((parent) => {
-        const from = nodes.get(parent) as INode;
+        const from = nodes.get(parent) as NodeModel;
         g.createEdge([from, to]);
       });
     }
